0379a2609e4e63ed6222aec03e589e71371fc60b
Josef Simonson <josef.simonson@meinfernbus.de>
2016-05-02 22:38:31 +0200

    Player: Add documentation

--- player/player.go
+++ player/player.go
@@ -3,7 +3,13 @@ package player
 import "github.com/lean-poker/poker-player-go/leanpoker"
 
 type Player interface {
+	
+	// BetRequest is invoked when the Lean Poker sends the current game state to the Bot.
 	BetRequest(state *leanpoker.Game) int
+	
+	// Showdown is invoked when a Showdown happens on the Lean Poker game server.
 	Showdown(state *leanpoker.Game)
+	
+	// Version is invoked when the Lean Poker game server requests identity of the version of the Bot.
 	Version() string
 }
