309a8b2e61b5aa4e17b7ed89043b79b149d13af3
Josef Simonson <josef.simonson@meinfernbus.de>
2016-05-02 20:55:05 +0200

    Player: Use player as an Interface

--- player/player.go
+++ player/player.go
@@ -2,16 +2,8 @@ package player
 
 import "github.com/lean-poker/poker-player-go/leanpoker"
 
-const VERSION = "Default Go folding player"
-
-func BetRequest(state *leanpoker.Game) int {
-	return 0
-}
-
-func Showdown(state *leanpoker.Game) {
-
-}
-
-func Version() string {
-	return VERSION
+type Player interface {
+	BetRequest(state *leanpoker.Game) int
+	Showdown(state *leanpoker.Game)
+	Version() string
 }
